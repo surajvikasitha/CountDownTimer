54a26282bcafe862070dd8cd6d9678aa71622dd3
surajvikasitha <clashofsura@gmail.com>
2019-03-13 14:41:35 +0530

    add

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -4,36 +4,67 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
-    private int counter=99;
-
+    private int countDown = 99;
+    private static final String CURRENT_COUNTER = "countDown" ;
+    private boolean wasRunning = false;
+    private boolean running = false ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        countDown();
+        if(savedInstanceState != null){
+            countDown = savedInstanceState.getInt(CURRENT_COUNTER);
+        }
+        CountDown();
+        running= true;
     }
-
-    private void countDown(){
-        final TextView textView=findViewById(R.id.textView);
-
-        final Handler handler=new Handler();
+    // 11-03-2019
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState );
+        savedInstanceState.putInt(CURRENT_COUNTER,countDown);
+    }
+    private void CountDown() {
+        final TextView textView = findViewById(R.id.textView);
+        //  textView.setText("Hello"); Use this to Check for initial checking
+        final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-
-                    textView.setText(Integer.toString(counter));
-                    counter--;
-                    handler.postDelayed(this,1000);
-
-                    if(counter==0){
-                        counter=99;
-                    }
+                textView.setText(Integer.toString(countDown));
+                // If u put the if condition in here U'll have to replace -1 with 0
+                if (running == true) {
+                    countDown--;
+                }
+                handler.postDelayed(this, 1000);
+                //---
+                if (countDown == -1) {
+                    countDown = 100; // Resetting the Counter
+                }
             }
         });
     }
-
+    protected void StartCount (View view ){
+        running = true;
+    }
+    protected void StopCount (View view){
+        running = false;
+    }
+    // 13-03-2019
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+    @Override
+    public void onStart(){ // You can use onRestart as well
+        super.onStart();
+        running=false;
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        wasRunning = true;
+        running= false;
+    }
 }
